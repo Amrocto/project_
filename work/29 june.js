6961bb7a78fe18510f95b1b2340970a9b264377b
Niyaz <ya.ya-niyaz2019@mail.ru>
2024-07-03 08:26:05 +0300

    second

--- work/29 june.js
+++ work/29 june.js
@@ -1,9 +1,25 @@
-function plus(x) {
-
-    return x+1;
-
+"use strict";
+let a=15;
+console.log(a);
+let number=5;
+{
+    var result="hello";
 }
-let y=4;
-plus(y);
-5;
-console.log(5);
+console.log(result);
+
+let incr = 10, decr = 10;
+console.log(incr++);
+console.log(decr--);
+// Выходом будет 10 с использованием постпрефикса 
+console.log(--decr);
+console.log(++incr);
+// Выходом будет 8 и 12 с использованием префикса
+
+console.log(5%2);
+// => 1
+
+console.log (5+2 == 3);
+// => false
+
+let carWidth = 50;
+
